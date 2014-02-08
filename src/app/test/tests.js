a9776ce8651c68940a05ad354dfae489747e87f3
MookieFumi <mookiefumi@outook.com>
2014-02-08 20:19:56 +0100

    Initial Commit

---
+++ src/app/test/tests.js
@@ -0,0 +1,6 @@
+﻿test("plugin defaults", function () {
+    var defaults = $.notifyCookiesPolicy.defaults;
+    ok(defaults.cookieName == "notifyCookiesPolicy_accepted", "Default cookie name");
+    ok(defaults.defaultScroll == 20, "Default scroll");
+    ok(defaults.cssClass == "notify-cookies-policy-container", "Default css class");
+});
